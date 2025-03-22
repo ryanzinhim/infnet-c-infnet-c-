3648289edecff9925cbf4ffbaf6c516efa596869
ryanzinhim <ryanvictor2019@outlook.com>
2025-03-22 20:36:57 -0300

    subindo certo

--- obj/Debug/net8.0/Infnet c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Infnet c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infnet c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1fe27b3a924ad087dc0841ec46bbd387f5fafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b999845d7831225da9c9490631a77ebac2c655d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infnet c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infnet c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
